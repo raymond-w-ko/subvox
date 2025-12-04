bc2d01be62986790054d1871d1a548f60b6ab31d
Raymond W. Ko <raymond.w.ko@gmail.com>
2025-12-04 13:28:52 -0500

    ghostty: trying out better different color cursor

--- home/.config/ghostty/shaders/cursor_smear.glsl
+++ home/.config/ghostty/shaders/cursor_smear.glsl
@@ -67,8 +67,9 @@ float ease(float x) {
 // const vec4 TRAIL_COLOR = vec4(0.914, 0.702, 0.992, 1.0); // light cursor
 // const vec4 TRAIL_COLOR = vec4(0.016, 0.82, 0.976, 1.0); // cyan
 // const vec4 TRAIL_COLOR = vec4(0.216, 0.957, 0.6, 1.0); // green
-const vec4 TRAIL_COLOR = vec4(0.0, 0.0, 0.0, 0.33); // black
-// const float OPACITY = 0.6; // this doesn't do anything right now
+// const vec4 TRAIL_COLOR = vec4(0.0, 0.0, 0.0, 0.33); // black
+const vec4 TRAIL_COLOR = vec4(1.0, 1.0, 0.0, 1.00); // highlighter yellow
+const float OPACITY = 0.6;
 const float DURATION = 0.10; //IN SECONDS
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord)
@@ -113,6 +114,6 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
     // Draw current cursor
     newColor = mix(newColor, TRAIL_COLOR, antialising(sdfCurrentCursor));
     newColor = mix(newColor, fragColor, step(sdfCurrentCursor, 0.));
-    // newColor = mix(fragColor, newColor, OPACITY);
+    newColor = mix(fragColor, newColor, OPACITY);
     fragColor = mix(fragColor, newColor, step(sdfCurrentCursor, easedProgress * lineLength));
 }
