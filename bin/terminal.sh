af2fd3d9efe49fdc3b5dfda22350af3dffbdff33
Raymond W. Ko <raymond.w.ko@gmail.com>
2025-12-05 20:13:50 -0500

    fix: wrap ghostty in gstreamer shell so bells work

--- bin/terminal.sh
+++ bin/terminal.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env -S bash -l
-setsid -f ghostty </dev/null >/dev/null 2>&1 || exit 1
+setsid -f nix-shell -p gst_all_1.gstreamer gst_all_1.gst-plugins-base gst_all_1.gst-plugins-good gst_all_1.gst-plugins-bad gst_all_1.gst-plugins-ugly gst_all_1.gst-libav gst_all_1.gst-vaapi --run "ghostty" </dev/null >/dev/null 2>&1 || exit 1
 exit 0
