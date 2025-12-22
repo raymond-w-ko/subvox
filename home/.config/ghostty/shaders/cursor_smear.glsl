1dcca4e434416c5b479924bcf710af5bb9a3f128
Raymond W. Ko <raymond.w.ko@gmail.com>
2025-12-22 13:24:12 -0500

    feat(ghostty): update cursor smear trail to green with longer duration

--- home/.config/ghostty/shaders/cursor_smear.glsl
+++ home/.config/ghostty/shaders/cursor_smear.glsl
@@ -68,9 +68,9 @@ float ease(float x) {
 // const vec4 TRAIL_COLOR = vec4(0.016, 0.82, 0.976, 1.0); // cyan
 // const vec4 TRAIL_COLOR = vec4(0.216, 0.957, 0.6, 1.0); // green
 // const vec4 TRAIL_COLOR = vec4(0.0, 0.0, 0.0, 0.33); // black
-const vec4 TRAIL_COLOR = vec4(0.67, 1.0, 0.0, 1.00); // highlighter yellow
+const vec4 TRAIL_COLOR = vec4(0.0, 1.0, 0.0, 1.00); // highlighter green
 const float OPACITY = 0.6;
-const float DURATION = 0.10; //IN SECONDS
+const float DURATION = 0.33; //IN SECONDS
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord)
 {
